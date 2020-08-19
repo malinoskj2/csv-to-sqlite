0cc8fc4c2d6b705f72e4e32182a4f1430a6d9180
Jesse Malinosky <jesse@malinoskj2.dev>
2020-08-19 12:37:23 -0400

    Add Driver, main

--- src/main/java/dev/malinoskj2/csvtosqlite/Driver.java
+++ src/main/java/dev/malinoskj2/csvtosqlite/Driver.java
@@ -1,9 +1,46 @@
 package dev.malinoskj2.csvtosqlite;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Driver {
+
+    // args[0] = CSV Path
+    public static void main(String[] args) {
+
+        File csv = new File(args[0]);
+
+        // register jdbc driver
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Could not load sqlite JDBC driver");
+        }
+
+        if (csv.exists()) {
+            String dbPath = args[0].replaceAll("\\.csv$", ".db");
+            String csvBadPath = args[0].replaceAll("\\.csv$", "-bad.csv");
+
+            try (Connection conn = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+                 BufferedWriter badCsvWriter = Files.newBufferedWriter(Paths.get(csvBadPath))) {
+
+                initTable(conn);
+
+                CsvProcessor processor = new CsvProcessor(csv, conn, badCsvWriter);
+                processor.process();
+            } catch (SQLException | IOException e) {
+                System.out.println(e.getMessage());
+            }
+        } else {
+            System.out.println("Please provide the path to a csv file.");
+        }
+    }
 
     public static void initTable(Connection con) throws SQLException {
         String sql = "CREATE TABLE IF NOT EXISTS users (\n"
