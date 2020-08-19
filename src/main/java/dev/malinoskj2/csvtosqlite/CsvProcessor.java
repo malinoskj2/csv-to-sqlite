4f194c46eedfb60b72c57ee4a7a8c53b0a51fa4a
Jesse Malinosky <jesse@malinoskj2.dev>
2020-08-19 12:44:35 -0400

    Add CsvProcessor, Constructors

--- src/main/java/dev/malinoskj2/csvtosqlite/CsvProcessor.java
+++ src/main/java/dev/malinoskj2/csvtosqlite/CsvProcessor.java
@@ -1,16 +1,31 @@
 package dev.malinoskj2.csvtosqlite;
 
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Connection;
 
 public class CsvProcessor {
 
     private final String csvPath;
+    private final Connection conn;
+    private final BufferedWriter badCsvWriter;
 
     private int numSuccessful = 0;
     private int numFailed = 0;
+
+    public CsvProcessor(File csv, Connection conn, BufferedWriter badCsvWriter) {
+        this(csv.getAbsolutePath(), conn, badCsvWriter);
+    }
+
+    public CsvProcessor(String csv, Connection conn, BufferedWriter badCsvWriter) {
+        this.csvPath = csv;
+        this.conn = conn;
+        this.badCsvWriter = badCsvWriter;
+    }
 
     private void writeLog() throws IOException {
         String logOutput = String.format("%d %s%n%d %s%n%d %s%n",
